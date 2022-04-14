289c6bdc8d9ff56be64c090aad88398dd5f0e057
Emil-Ka <emilkaraev55@gmail.com>
2022-04-14 10:02:58 +0300

    fixed styles

---
+++ src/components/Popup/Popup.tsx
@@ -0,0 +1,68 @@
+import cx from 'classnames';
+
+import {GlobalSvgSelector} from '../../assets/icons/global';
+import {IndicatorsSvgSelector} from '../../assets/icons/indicators';
+
+import styles from './Popup.module.scss';
+
+export const Popup = () => {
+  const active = false;
+  return (
+    <div className={cx(styles.background, {
+      [styles.background_active]: active
+    })}>
+      <div className={styles.popup}>
+        <div className={styles.closeIcon}>
+          <GlobalSvgSelector id="close"/>
+        </div>
+        <div className={styles.popupInner}>
+          <div className={styles.locationInfo}>
+            <p className={styles.temp}>+21°</p>
+            <p className={styles.dayWeek}>Сб</p>
+            <GlobalSvgSelector id="rain-sun"/>
+            <p className={styles.date}>21 авг</p>
+            <p className={styles.city}>Город: Санкт-Петербург</p>
+          </div>
+          <ul className={styles.indicators}>
+            <li>
+              <div className={styles.indicator}>
+                <div className={styles.round}>
+                  <IndicatorsSvgSelector id="temp"/>
+                </div>
+                <p className={styles.indicatorTitle}>Температура</p>
+              </div>
+              <p className={styles.weatherDesc}>20° - ощущается как 17°</p>
+            </li>
+            <li>
+              <div className={styles.indicator}>
+                <div className={styles.round}>
+                  <IndicatorsSvgSelector id="humidity"/>
+                </div>
+                <p className={styles.indicatorTitle}>Давление</p>
+              </div>
+              <p className={styles.weatherDesc}>765 мм ртутного столба - нормальное</p>
+            </li>
+            <li>
+              <div className={styles.indicator}>
+                <div className={styles.round}>
+                  <IndicatorsSvgSelector id="pressure"/>
+                </div>
+                <p className={styles.indicatorTitle}>Осадки</p>
+              </div>
+              <p className={styles.weatherDesc}>Без осадков</p>
+            </li>
+            <li>
+              <div className={styles.indicator}>
+                <div className={styles.round}>
+                  <IndicatorsSvgSelector id="wind"/>
+                </div>
+                <p className={styles.indicatorTitle}>Ветер</p>
+              </div>
+              <p className={styles.weatherDesc}>3 м/с юго-запад - легкий ветер</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import {Header} from '../../components/Header';
 import {BriefForecast} from '../../components/BriefForecast';
 import {ForecastInfo} from '../../components/ForecastInfo';
 import {DaysForecast} from '../../components/DaysForecast';
+import {Popup} from '../../components/Popup';
 
 import styles from './Home.module.scss';
 
 export const Home = () => {
   return (
     <div className={styles.home}>
+      <Popup/>
       <div className={styles.wrapper}>
         <Header/>
         <div className={styles.oneDayInfo}>
